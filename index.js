f2dd967e0c54081ce1b3be542b4a726bd64bbd49
Vasco Ramos <vasco@tyba.com>
2016-06-29 11:46:13 +0200

    Final stuff

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var github = require('octonode');
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const github = require('octonode');
+const app = express();
+
+// Config
+
+// Create config file with this later
+const dotenv = require('dotenv')
+const ENV = process.env.NODE_ENV || 'development'
+
+if (ENV === 'development') dotenv.load()
+
+// end of Config
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -14,14 +26,27 @@ app.get('/', function(req, res){
 
 app.post('/close', function(req, res){
 
+    if(!req.body.number){
+        console.log(req.body);
+        return
+    }
+
+    // Close Issue
+    // If this file of webhooks grows put this into a function later
     var client = github.client(process.env.GITHUB_KEY);
     var ghissue = client.issue(process.env.ISSUES_REPO, req.body.number);
 
     ghissue.update({
         "state": "closed",
-    }, function(){
-        res.send('It is Working!');
+    }, function(err, data, headers) {
+        if(err) {
+            console.log(err);
+            res.send(err);
+            return
+        }
+        res.send(data)
     });
+    // End of Close issue
 
 });
 
